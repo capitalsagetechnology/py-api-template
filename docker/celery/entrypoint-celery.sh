e0e11ec8357f5f55e938d4dcdb6827658476ec52
Daniel Ale <danielale9291@gmail.com>
2025-03-31 00:03:59 +0100

    rf: add black and hooks

--- docker/celery/entrypoint-celery.sh
+++ docker/celery/entrypoint-celery.sh
@@ -3,9 +3,10 @@
 celery -A core worker --loglevel=info --concurrency=2 --hostname=worker1@%h --detach
 celery -A core worker --loglevel=info --concurrency=2 --hostname=worker2@%h --detach
 celery -A core worker --loglevel=info --concurrency=2 --hostname=worker3@%h --detach
-celery -A core worker --loglevel=info --concurrency=2 --hostname=worker4@%h --detach
 
-celery -A core beat -l debug --detach
+celery -A core worker --loglevel=info --queues=reversal_queue --concurrency=1 --hostname=reversal_worker@%h --prefetch-multiplier=1 --detach
+
+celery -A core beat -l INFO --detach
+celery -A core beat -l INFO --detach --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
 exec "$@"
-
-
